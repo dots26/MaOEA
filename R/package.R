6b93a2a8f94ca48173edecba1acc638817d6a909
Dani Irawan <irawan_dani@yahoo.com>
2019-07-12 18:53:32 +0200

    added package description. Removed gtools

---
+++ R/package.R
@@ -0,0 +1,45 @@
+# Package Description for Roxygen:
+#' MaOEA contains several algorithms for solving many-objective optimization problems.
+#' The algorithms are provided as a sequence of operators used in a single iteration.
+#' For example, the SMSEMOA function calls the recombination (SBX) and mutation operator (polynomial mutation) to produce 1 offspring, and perform the S-metric selection.
+#' The function then returns a list containing the population and population objective after the procedure is conducted once.
+#' The purpose of only doing a single iteration is to support users if they wish to formulate hybrid algorithms.
+#'
+#' Alternatively, users can use the optimMaOEA function to solve an optimization problem with their chosen algorithm.
+#' This function is a simple wrapper to call the algorithms listed above for several iterations.
+#' Using this function, users can simply supply the initial population, objective function, the chosen algorithm, and the number of iterations. If number of iteration is not supplied, then only a single iteration is conducted.
+#'
+#' \tabular{ll}{
+#' Package: \tab MaOEA\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.4.1\cr
+#' Date: \tab 2019-07-12\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' @name MaOEA-package
+#' @aliases MaOEA
+#' @docType package
+#' @title Many-Objective Evolutionary Algorithm
+#' @author Dani Irawan \email{irawan_dani@@yahoo.com}
+#' @keywords package
+#' @seealso Main interface function is \code{\link{optimMaOEA}}.
+#' @import reticulate
+#' @import nsga2R
+#' @import lhs
+#' @import nnet
+#' @import stringr
+#' @import randtoolbox
+#' @import e1071
+#'
+#' @section Acknowledgments:
+#' This work is funded by the European Commission's H2020 programme through
+#' the UTOPIAE  Marie  Curie  Innovative  Training Network, H2020-MSCA-ITN-2016,
+#' under Grant Agreement No. 722734 as well as  through the Twinning project SYNERGY
+#' under Grant Agreement No. 692286.
+#'
+#' @section Maintainer:
+#' Dani Irawan \email{irawan_dani@@yahoo.com}
+#End of Package Description
+NA
