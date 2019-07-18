e87183ad1f81b25bc485caf3ffae2193d9863d6c
Dani Irawan <irawan_dani@yahoo.com>
2019-07-18 12:13:27 +0200

    added evopath and covariance matrix as possible parameter

--- R/cmaes_gen.R
+++ R/cmaes_gen.R
@@ -3,6 +3,8 @@
 #' @param population The number of objective functions. A scalar value.
 #' @param ps_target The target success rate. Used to initialize cmaes_gen$averageSuccessRate.
 #' @param stepSize The initial step size.
+#' @param evoPath A vector of numbers indicating evolution path of each variable.
+#' @param covarianceMatrix Covariance matrix of the variables.
 #' @return An object of cmaes_gen class. It can be used as MO-CMA-ES parent. It is a 5 tuple: x (the design point, length = number of variable),averageSuccessRate (scalar),stepSize (scalar), evoPath (evolution path, vector, length = number of variable ),covarianceMatrix (square matrix with ncol = nrow = number of variable).
 #' @examples
 #' nVar <- 14
@@ -10,9 +12,12 @@
 #' nIndividual <- 100
 #' crossoverProbability <- 1
 #' ps_target <- 1 / (5 + ( 1 / 2  )^0.5 )
-#' pop <- matrix(runif(nIndividual*nVar), nrow = nVar) # create the population
+#' pop <- matrix(stats::runif(nIndividual*nVar), nrow = nVar) # create the population
 #' a_list <- cmaes_gen(pop)
-#' newGeneration <- CMAES(a_list,nObjective,"WFG8",ps_target,crossoverProbability,TRUE) # will run a generation of MO-CMA-ES with standard WFG8 test function.
+#' control <- list(successProbTarget=ps_target,crossoverProbability=crossoverProbability)
+#'
+#' # run a generation of MO-CMA-ES with standard WFG8 test function.
+#' newGeneration <- MOCMAES(a_list,nObjective,WFG8,control,nObjective)
 #' @export
 cmaes_gen <- function(population,ps_target= (1 / (5 + ( 1 / 2  )^0.5)),stepSize=0.5,evoPath = rep(0,nrow(population)),
                       covarianceMatrix = diag(nrow(population)) ){
