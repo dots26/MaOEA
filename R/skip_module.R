c02b93df28b229cfd8c155969edc7e4dc906d1c3
Dani Irawan <irawan_dani@yahoo.com>
2019-08-14 16:24:40 +0200

    remived print

--- R/skip_module.R
+++ R/skip_module.R
@@ -1,11 +1,11 @@
 skip_if_missing_module <- function() {
   have_numpy <- reticulate::py_module_available("numpy")
   if (!have_numpy)
-    print("numpy not available for testing")
+    testthat::skip("numpy not available for testing")
 
   have_pygmo <- reticulate::py_module_available("pygmo")
   if (!have_pygmo)
-    print("pygmo not available for testing")
+    testthat::skip("pygmo not available for testing")
 
   if(!have_numpy || !have_pygmo)
     testthat::skip("Missing required python modules, skipping test.")
