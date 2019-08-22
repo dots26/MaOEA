f1c6fa34a4b750f368ee3bc5e75e02bd1d485f0c
Dani Irawan <irawan_dani@yahoo.com>
2019-08-22 11:23:30 +0200

    moved have_pygmo check earlier

--- R/skip_module.R
+++ R/skip_module.R
@@ -1,9 +1,9 @@
 skip_if_missing_module <- function() {
-  have_numpy <- reticulate::py_module_available("numpy")
+  have_numpy <<- reticulate::py_module_available("numpy")
+  have_pygmo <<- reticulate::py_module_available("pygmo")
   if (!have_numpy)
     testthat::skip("numpy not available for testing")
 
-  have_pygmo <- reticulate::py_module_available("pygmo")
   if (!have_pygmo)
     testthat::skip("pygmo not available for testing")
 
